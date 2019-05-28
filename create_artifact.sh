1a72f0ce77219b5f1fac9263c85072a7761756f9
Mike Long <mike@praqma.com>
2019-05-28 09:34:37 +0200

    Add a script to insert artifact information to compliancedb

---
+++ create_artifact.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# USAGE:
+# $1 organization
+# $2 project
+# $3 sha
+# $4 filename
+# $5 description
+#
+# ./create_artifact.sh cern hadroncollider '084c799cd551dd1d8d5c5f9a5d593b2e931f5e36122ee5c793c1d08a19839cc0' accelerator.jar 'My artifact created by build number 1337 on server X'
+
+
+curl -H 'Content-Type: application/json' \
+     -X PUT \
+     -d '{"sha256": "'"$3"'", "filename": "'"$4"'", "description": "'"$5"'"}' \
+    http://hub/api/projects/$1/$2/artifacts
