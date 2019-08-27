bdfa48128dc8ef6300fa10808d1e1f24fa250cc2
Mike Long <mil@praqma.net>
2019-08-27 20:13:59 +0200

    Add links to build and git commit in pipeline

--- create_artifact.sh
+++ create_artifact.sh
@@ -7,11 +7,13 @@
 # $4 filename
 # $5 description
 # $6 git_sha
+# $7 commit_url
+# $8 build_url
 #
-# ./create_artifact.sh cern hadroncollider '084c799cd551dd1d8d5c5f9a5d593b2e931f5e36122ee5c793c1d08a19839cc0' accelerator.jar 'My artifact created by build number 1337 on server X'
+# ./create_artifact.sh cern hadroncollider '084c799cd551dd1d8d5c5f9a5d593b2e931f5e36122ee5c793c1d08a19839cc0' accelerator.jar 'My artifact created by build number 1337 on server X' http://jenkins.mycorp.com/bananas/
 
 
 curl -H 'Content-Type: application/json' \
      -X PUT \
-     -d '{"sha256": "'"$3"'", "filename": "'"$4"'", "description": "'"$5"'", "git_commit": "'"$6"'"}' \
+     -d '{"sha256": "'"$3"'", "filename": "'"$4"'", "description": "'"$5"'", "git_commit": "'"$6"'", "commit_url": "'"$7"'", "build_url": "'"$8"'"}' \
     http://hub/api/projects/$1/$2/artifacts
