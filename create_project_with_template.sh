ff67e6c95c841026edcdd236677ed2fb3f0363e1
Mike Long <mike@praqma.com>
2019-11-25 11:46:21 +0100

    Create project with template example script

---
+++ create_project_with_template.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# USAGE:
+# $1 host
+# $2 name
+# $3 description
+# $4 owner
+#
+# ./create_project_with_template.sh localhost hadroncollider, "Accelerating particles then smashing them together", "cern"
+
+curl -H 'Content-Type: application/json' \
+     -X PUT \
+     -d '{"name": "'"$2"'", "description": "'"$3"'", "owner": "'"$4"'", "template": ["artifact", "code_review", "integration_test", "security_scan"]}' \
+    $1/api/projects/$4
